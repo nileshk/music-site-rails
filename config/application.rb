98d8b11f75e0fdbb3739078ca89c76921b6f13c0
Nilesh Kapadia <code@nileshk.com>
2010-09-29 22:22:04 -0400

    Commented out action_mailer config

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,6 @@ module MusicSite
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   end
 end
