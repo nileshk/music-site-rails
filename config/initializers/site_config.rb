cb6f225b860fbb9ba7df2f3515ccc420dfead6ca
Nilesh Kapadia <code@nileshk.com>
2010-10-03 15:53:27 -0400

    Use a configured author URL to hyperlink copyrighted author

--- config/initializers/site_config.rb
+++ config/initializers/site_config.rb
@@ -2,4 +2,5 @@ require "ostruct"
 
 SiteConfig = OpenStruct.new({:sitename => "Nilesh D Kapadia's Music",
                               :author => "Nilesh D Kapadia",
+                              :author_url => "http://nileshk.com",
                               :google_analytics_account => "UA-18049880-2"})
